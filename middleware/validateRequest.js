31c4d72cb7100add2e29dd7ee83e0172d21ccb34
Pranto Deb <rajprit0m@outlook.com>
2024-12-12 02:53:42 +0600

    [Auth] : validate request handle, auth controller created

---
+++ middleware/validateRequest.js
@@ -0,0 +1,12 @@
+const validateRequest = (req, res, next) => {
+  const errors = [];
+  if (!req.body.name) errors.push("Name is required");
+  if (!req.body.email) errors.push("Email is required");
+  if (!req.body.password) errors.push("Password is required");
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+  next();
+};
+
+module.exports = validateRequest;
